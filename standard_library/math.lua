f09d251488ed62457ef4612a5c27f59cb865a5c9
serr <sc7227484@gmail.com>
2025-07-25 17:11:34 +0300

    math

---
+++ standard_library/math.lua
@@ -0,0 +1,6 @@
+
+print(math.pi)
+print(math.cos(math.pi)) --> -1
+print(math.cos(math.rad(45))) --> 0.70710678118655 (косинус 45 градусов)
+
+-- Функция math.random генерирует псевдослучайные числа
