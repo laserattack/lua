9f18dfc95020d11899ec2a2da8d9a7dd218ea7b5
serr <sc7227484@gmail.com>
2025-07-28 22:37:19 +0300

    пример работы с luarocks

---
+++ src/luarocks/proj_example/proj.lua
@@ -0,0 +1,40 @@
+
+-- Чтобы установить пакет (например, luasocket) в подпапку проекта (например, lua_modules), 
+-- выполните: luarocks install --tree ./lua_modules luasocket
+
+-- запускается через luajit -l set_paths proj.lua
+
+local luasocket = require("socket")
+
+for k, v in pairs(luasocket) do print(k) end
+
+-- tcp
+-- _DATAGRAMSIZE
+-- _SETSIZE
+-- _SOCKETINVALID
+-- newtry
+-- connect4
+-- source
+-- sink
+-- connect
+-- sinkt
+-- connect6
+-- sourcet
+-- bind
+-- BLOCKSIZE
+-- choose
+-- try
+-- sleep
+-- udp6
+-- tcp6
+-- dns
+-- select
+-- skip
+-- __unload
+-- _DEBUG
+-- _VERSION
+-- protect
+-- gettime
+-- tcp4
+-- udp
+-- udp4
