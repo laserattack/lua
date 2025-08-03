933d344ce1011cb4841d396193321e28829a65b9
serr <sc7227484@gmail.com>
2025-08-03 20:20:28 +0300

    some changes

--- src/recipes/signals_without_uv.lua
+++ src/recipes/signals_without_uv.lua
@@ -10,20 +10,15 @@ ffi.cdef[[
     static const int SIGINT = 2;
 ]]
 
--- Функция-обработчик сигнала (вызывается при SIGINT)
 local function sigint_handler(signum)
     print("Signal handled:", signum)
     os.exit(0)
 end
 
--- Получаем указатель на Lua-функцию для передачи в C
 local handler = ffi.cast("sighandler_t", sigint_handler)
-
--- Устанавливаем новый обработчик для SIGINT (Ctrl+C)
 ffi.C.signal(ffi.C.SIGINT, handler)
 
--- Бесконечный цикл для демонстрации
 print("Press Ctrl+C to exit...")
 while true do
-    ffi.C.getchar() -- Ожидаем ввод (необязательно)
+    ffi.C.getchar()
 end
