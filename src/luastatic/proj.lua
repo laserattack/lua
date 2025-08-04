ac5c2c974f62ed767c973b4b1d0b8bab5eff1d7f
serr <sc7227484@gmail.com>
2025-08-04 12:20:42 +0300

    some changes

--- src/luastatic/proj.lua
+++ src/luastatic/proj.lua
@@ -1,7 +1,9 @@
+-- Сначала через luarocks надо поставить luastatic в ./lua_modules
+
+-- Собирать исполняемый файл так:
+
 -- С зависимостями от libc:
 -- ./lua_modules/bin/luastatic proj.lua /usr/local/lib/libluajit-5.1.a -I/usr/local/include/luajit-2.1/
-
-
 -- Полностью статический бинарник:
 -- ./lua_modules/bin/luastatic proj.lua /usr/local/lib/libluajit-5.1.a -I/usr/local/include/luajit-2.1/ -static -lm -ldl
 
